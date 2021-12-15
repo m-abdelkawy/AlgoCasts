c02f9124863b1be5cafcf801884fa4d86792adfb
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2021-12-15 01:42:25 -0600

    06. maxChars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {};
+    for (const char of str) {
+        chars[char] = chars[char] + 1 || 1;
+    }
+
+    let count = 0;
+    let char = '';
+
+    for (const key in chars) {
+        if (chars[key] >= count) {
+            count = chars[key];
+            char = key;
+
+        }
+    }
+
+    return char;
+}
+
+//console.log("maxchar: " + maxChar("abcccccccd"));
 
 module.exports = maxChar;
