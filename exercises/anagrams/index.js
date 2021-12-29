4ca1ccb03e8bb9c68d1aa071013c04227000ab07
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2021-12-29 16:41:08 -0600

    10. sentence capitalization

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,29 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    stringA.replace(/[^\w]/g, "").toLowerCase();
+    stringB.replace(/[^\w]/g, "").toLowerCase();
+
+    let map1 = {};
+    let map2 = {};
+
+    for (const char of stringA) {
+        map1[char] = map1[char] + 1 || 1;
+    }
+    for (const char of stringB) {
+        map2[char] = map2[char] + 1 || 1;
+    }
+
+    
+    if(Object.keys(map1).length != Object.keys(map2).length) return false;
+    for (const prop in map1) {
+        if(!map2.hasOwnProperty(prop) || map1[prop] != map2[prop]) return false;
+    }
+    return true;
+}
+
+
+// anagrams("abc", "bca");
 
 module.exports = anagrams;
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,62 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let res = str[0].toUpperCase();
+    for (let i = 1; i < str.length; i++) {
+        if(str[i-1] == ' '){
+            res += str[i].toUpperCase();
+        }else{
+            res += str[i];
+        }
+    }
+    return res;
+}
+
+console.log(capitalize('a short sentence'))
+
 
 module.exports = capitalize;
+
+
+//#region solution 1
+// let str2 = ' ' + str;
+//     let res = '';
+
+    
+//     let isFirstLetter = true;
+//     for(let c of str){
+//         if(isFirstLetter && c !== ' '){
+//             res += c.toUpperCase();
+//         }else{
+//             res += c;
+//         }
+//         if(c == ' '){
+//             isFirstLetter = true;
+//         }else{
+//             isFirstLetter = false;
+//         }
+//     }
+//     return res;
+//#endregion
+
+//#region  solution 2
+// let res = [];
+// let words = str.split(' ');
+// for (const word of words) {
+//     res.push(word[0].toUpperCase() + word.slice(1));
+// }
+// return res.join(' ');
+//#endregion
+
+//#region solution 3
+// let res = str[0].toUpperCase();
+// for (let i = 1; i < str.length; i++) {
+//     if(str[i-1] == ' '){
+//         res += str[i].toUpperCase();
+//     }else{
+//         res += str[i];
+//     }
+// }
+// return res;
+//#endregion
