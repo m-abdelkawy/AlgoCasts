fd3759e44d895f70a2247eb75efdb98abb19af8c
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2021-12-29 20:25:24 -0600

    11. Printing Steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,77 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    stepsRecursive(n, 0, '');
+}
+function stepsRecursive(n, row = 0, stair = '') {
+    //base case
+    if (row === n) return;
+
+    if (stair.length === n) {
+        console.log(stair);
+        return stepsRecursive(n, row + 1);
+    }
+
+    if (stair.length <= row)
+        stair += '#';
+    else
+        stair += ' ';
+    stepsRecursive(n, row, stair);
+}
+
+steps(4);
 
 module.exports = steps;
+
+
+//#region Solution1
+// for (let i = n; i > 0; i--) {
+//     let str = '';
+//     for (let j = n - i + 1; j > 0;j--) {
+//         str+="#";
+//     }
+//     for (let k = i-1; k >0; k--) {
+//         str+= ' ';
+//     }
+//     console.log(str);
+// }
+//#endregion
+
+//#region solution2
+// for (let row = 0; row < n; row++) {
+//     let stair = '';
+//     for (let col = 0; col < n; col++) {
+//         if(col <= row){
+//             stair += "#";
+//         }else{
+//             stair += ' ';
+//         }
+//     }
+//     console.log(stair);
+// }
+
+//#endregion
+
+//#region solution3
+// function steps(n) {
+//     printhash(n,n);
+// }
+// function printhash(n, level){
+//     //base case
+//     if(level === 0) return;
+//     let str = '';
+//     for (let i = 0; i < n-level+1; i++) {
+//         str+="#";
+//     }
+//     for (let j = 0; j < level-1; j++) {
+//         str += ' ';
+//     }
+//     console.log(str);
+//     printhash(n, level-1);
+// }
+//#endregion
+
+//#region solution 4
+
+//#endregion
