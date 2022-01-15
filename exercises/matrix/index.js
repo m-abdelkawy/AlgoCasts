a115c57c8709f95b9e4d02b7308a10ff66ebbf46
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2022-01-15 08:11:36 -0600

    14. Enter the matrix spiral

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,40 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let num = 0;
+    const res = [];
+    for (let i = 0; i < n; i++) {
+        res[i] = [];
+    }
+    let stCol = 0;
+    let endCol = n-1;
+    let stRow = 0;
+    let endRow = n-1;
+    while(stCol<=endCol && stRow<=endRow){
+        for (let i = stCol; i <= endCol; i++) {
+            res[stRow][i] = ++num;
+        }
+        stRow++;
+
+        for (let i = stRow; i <= endRow; i++) {
+            res[i][endCol] = ++num;
+        }
+        endCol--;
+
+        if(!(stCol<=endCol) || !(stRow<=endRow)) break;
+
+        for (let i = endCol; i >= stCol; i--) {
+            res[endRow][i] = ++num;
+        }
+        endRow--;
+
+        for (let i = endRow; i >= stRow; i--) {
+            res[i][stCol] = ++num;
+        }
+        stCol++;
+    }
+    return res;
+}
 
 module.exports = matrix;
